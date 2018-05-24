e2304e4a7714b52a23a2251b27b3ed172a36fcc6
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-05-24 15:10:09 +0200

    fonctionnelle

--- autolib_csv/findautolib.py
+++ autolib_csv/findautolib.py
@@ -3,6 +3,7 @@
 import csv
 import codecs
 
+"""
 # maliste = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 mondict = {}
 
@@ -21,3 +22,22 @@ with codecs.open ('./autolib.csv', 'rU', encoding = "utf-8", errors = "ignore") 
 # for i in maliste:
 #     print( i )
 print( mondict )
+"""
+
+# Methode fonctionnelle 
+
+from itertools import groupby
+
+def OpenCsvAsDict( path ):
+    with codecs.open (path, 'rU', encoding = "utf-8", errors = "ignore") as csvfile:
+        return list( csv.DictReader( csvfile, delimiter=';' ) )
+
+reader = OpenCsvAsDict( './autolib.csv' )
+filteredReader = filter( lambda x: x[ 'Postal code' ][:3] == "750", reader )
+reduced = list( map( lambda x: ( x[ 'Postal code' ], x[ 'Cars' ] ), filteredReader ) )
+sortedList = list( sorted( reduced, key=lambda x: x[ 0 ] ) )
+grouped = groupby( sortedList, key=lambda x: x[ 0 ] )
+grouped2 = list( map( lambda x: ( x[ 0 ], list( x[ 1 ] ) ), grouped ) )
+final = list( map( lambda x: ( x[ 0 ], sum( map( lambda y: int( y[ 1 ] ) , x[ 1 ] ) ) ), grouped2 ) )
+
+print( final )
